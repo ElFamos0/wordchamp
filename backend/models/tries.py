ae38fb75c13f246d45991fc8b7c7f1f9eef89ac5
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-04-25 10:24:56 +0200

    [MODIF] suppr de l'attribut game_type de Tries

--- backend/models/tries.py
+++ backend/models/tries.py
@@ -8,14 +8,12 @@ class Tries(db.Model):
     try_number = db.Column(db.Integer,nullable=False)
     word = db.Column(db.String(40), nullable=False)
     id_game = db.Column(db.String(40), db.ForeignKey("games.id"),nullable=False)
-    game_type = db.Column(db.String(40), nullable=False)
 
    
 
-    def __init__(self, id_game,game_type,word,try_number):
+    def __init__(self, id_game,word,try_number):
         self.id = uuid4().hex
         self.id_game = id_game
-        self.type = game_type
         self.word = word
         self.try_number = try_number
 
