7eae8b8ad0ab29c75bca5f1063640eb7d2cd3038
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-04-27 20:04:43 +0200

    [AJOUT] Sliders sur Choice et components Sliders

---
+++ frontend/src/components/ChoiceSlider.vue
@@ -0,0 +1,34 @@
+<template v-slot:thumb-label='{value}'>
+    <v-slider
+            v-model="slider"
+            :thumb-size=thumbSize
+            :thumb-label=thumbLabel
+            step='1'
+            ticks
+            :max=max
+            :min=min
+            :messages=message
+            color=color
+        >
+    </v-slider>
+</template>
+
+<script>
+export default {
+    props:{
+        max: Number,
+        min: Number,
+        message: String,
+        thumbLabel: String,
+        thumbSize: String,
+        color: String,
+
+    },
+    data() {
+        return {
+            slider: 0,
+            value: 0,
+        }
+    }
+}
+</script>
--- frontend/src/views/Choice.vue
+++ frontend/src/views/Choice.vue
@@ -1,16 +1,27 @@
 <script>
+import ChoiceSlider from '../components/ChoiceSlider.vue'
 export default {
+    data () {
+      return {
+        nbLettres: 5,
+        nbTentatives: 1,
+      }
+    },
     name: 'choice-page',
     methods: { 
         onClick : function(i) {
-            this.$router.push('/word/:'+(i+4))
+            this.$router.push('/word/:'+(i))
         }       
     },
+    components: {
+        ChoiceSlider
+    }
 }
 </script>
 
 <template>
-<div style="padding-top: 2%">
+
+<!-- <div style="padding-top: 2%">
     <h1 style="font-family: 'Roboto', sans-serif">
         Choisis le nombre de lettres
     </h1>
@@ -21,7 +32,29 @@ export default {
             </v-btn>
         </div>
     </v-container>
-</div>
+</div> -->
+ <v-card
+    flat
+    color="transparent"
+    width="80vw"
+    style="margin-left: auto; margin-right: auto; margin-top: 7%"
+  >
+    <!-- <v-subheader>Nombre de lettres</v-subheader> -->
+        <h3 style="font-family: 'Roboto', sans-serif">Sélectionne un nombre de lettres</h3>
+        <choice-slider v-model=nbLettres min=5 max=9 thumbSize='24' thumbLabel="true" message="(entre 5 et 9)" style="padding-bottom: 4%"/>
+
+    <!-- <v-subheader>Nombre de tentatives</v-subheader> -->
+        <h3 style="font-family: 'Roboto', sans-serif">Sélectionne un nombre de tentatives</h3>
+        <choice-slider v-model=nbTentatives min=1 max=10 thumbSize='24' thumbLabel="true" message="(max. 10)" style="padding-bottom: 4%"/>
+        <v-container style="display: flex; justify-content: center">
+        <div class="my-2">
+            <v-btn @click="onClick(nbLettres)" x-small color="primary">
+                C'est parti !
+            </v-btn>
+        </div>
+    </v-container>
+  </v-card>
+  
 </template>
 
 <style scoped>
