7019519e9fe25d54ff58d655fad13769b8d8b258
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-04-27 09:29:35 +0200

    [STYLE] GRADIENT

--- frontend/src/components/HistoryEntry.vue
+++ frontend/src/components/HistoryEntry.vue
@@ -19,8 +19,10 @@ export default {
   computed: {
     colorClass() {
       console.log(this.result)
-      let color = (this.result == 'Victoire' ) ? '#60b760' : '#ff4545';
-      return 'background-color: '+color;
+      let color1 = (this.result == 'Victoire' ) ? 'green' : 'red';
+      let color2 = (this.result == 'Victoire' ) ? 'rgb(118, 255, 127)' : 'rgb(255, 134, 134)';
+
+      return 'box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset; background:linear-gradient('+color1+', transparent),linear-gradient(to top left, '+color2+', transparent);background-blend-mode: screen;'
     }
   },
   components: {
