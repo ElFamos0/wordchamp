a89dec893a95df1f884a0e966c749bc43c6e7b66
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-04-29 18:00:16 +0200

    [MODIF] deux classements séparés

--- frontend/src/views/ClassementPage.vue
+++ frontend/src/views/ClassementPage.vue
@@ -1,6 +1,34 @@
 <template>
 <div>
-    <v-table theme="light" density="default" style="margin-bottom:5%; margin-left:10%; margin-right:10%; margin-top:5% ; border-radius:4px; box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;">
+    <v-tabs class="justify-content-center" style="margin: auto; max-width: 240px">
+        <v-tab><a href="javascript:void(0);" :class="{ 'router-link-exact-active': normal }" @click="normal=true; carriere=false"> Classique </a></v-tab>
+        <v-tab><a href="javascript:void(0);" :class="{ 'router-link-exact-active': carriere }" @click="normal=false; carriere=true"> Carrière </a></v-tab>
+    </v-tabs>
+    <v-table v-if="normal" theme="light" density="default" style="margin-bottom:5%; margin-left:10%; margin-right:10%; margin-top:5% ; border-radius:4px; box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;">
+        <template v-slot:default>
+        <thead style="background:rgb(50, 50, 50); color:white">
+            <tr>
+                <th class="text-center" style="padding-top:10px">
+                    <h3>#</h3>   
+                </th>
+                <th class="text-center" style="padding-top:10px">
+                    <h3>Pseudo</h3>
+                </th>
+                <th class="text-center" style="padding-top:10px">
+                    <h3>Points</h3>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="userData in usersData" :key="userData.classement" :class="{'rank-first': userData.classement == 1, 'rank-second': userData.classement == 2, 'rank-third': userData.classement == 3} "> 
+                <td>{{ userData.classement }}</td>
+                <td>{{ userData.username }}</td>
+                <td>{{ userData.points }}</td>
+            </tr>
+        </tbody>
+        </template>
+    </v-table>  
+    <v-table v-if="carriere" theme="light" density="default" style="margin-bottom:5%; margin-left:10%; margin-right:10%; margin-top:5% ; border-radius:4px; box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;">
         <template v-slot:default>
         <thead style="background:rgb(50, 50, 50); color:white">
             <tr>
@@ -13,9 +41,6 @@
                 <th class="text-center" style="padding-top:10px">
                     <h3>Elo</h3>
                 </th>
-                <th class="text-center" style="padding-top:10px">
-                    <h3>Points</h3>
-                </th>
             </tr>
         </thead>
         <tbody>
@@ -23,11 +48,10 @@
                 <td>{{ userData.classement }}</td>
                 <td>{{ userData.username }}</td>
                 <td>{{ userData.elo }}</td>
-                <td>{{ userData.points }}</td>
             </tr>
         </tbody>
         </template>
-    </v-table>    
+    </v-table>     
 </div>
 </template>
 
@@ -41,6 +65,8 @@ export default {
     data () {
         return {
             usersData: [],
+            normal: true,
+            carriere: false,
             }
     },
     created() {
